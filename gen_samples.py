97cb1882843028774d96e5f731c6ab1d086efe0d
olvb <none@nowhe.re>
2018-05-16 16:03:21 +0200

    rewrite samples script

--- gen_samples.py
+++ gen_samples.py
@@ -1,114 +1,84 @@
 #!/usr/bin/env python3
 
-## run all tests in tests/ directory
-## or with file given as arg (ex ./run-tests tests/01_decl_assign.py)
-
-import sys
 import os
 import subprocess
 import re
 
-BIN_PATH = "./bin/dot2png"
-DOTS_PATH = "samples/"
-PNGS_PATH = "samples/"
-THUMBS_PATH = "samples/thumbs/"
-ANIMS_PATH = "samples/thumbs/"
-FR_CMD = BIN_PATH + " -m fr {} {}"
-KK_CMD = BIN_PATH + " -m kk {} {}"
-GV_CMD = "neato -Nshape=point -Nwidth=0.1 -Gsize=10,7\! -Gdpi=100 -Tpng {} -o {}"
-GV_RESIZE_CMD = "convert {} -resize 930x700 -gravity center -extent 1024x768 {}"
-THUMB_CMD = "convert {} -resize 50% {}"
-ANIM_CMD = BIN_PATH + " -a -m fr {} {}"
-ANIM_COMPILE_CMD = "convert -colorspace gray -loop 256 -delay 5 {} -delay 300 {} -layers optimize {}"
+FR_CMD = './bin/dot2png -m fr samples/{0}.dot samples/{0}_fr.png'
+KK_CMD = './bin/dot2png -m kk samples/{0}.dot samples/{0}_kk.png'
+GV_CMD = 'neato -Nshape=point -Nwidth=0.1 -Gsize=10,7\! -Gdpi=100 -Tpng samples/{0}.dot -o samples/{0}_gv.png'
+GV_RESIZE_CMD = 'convert samples/{0}_gv.png -resize 930x700 -gravity center -extent 1024x768 samples/{0}_gv.png'
+THUMB_CMD = 'convert samples/{0}.png -resize 50% samples/thumbs/{0}.png'
+ANIM_CMD = './bin/dot2png -a -m fr samples/{0}.dot samples/{0}_frame.png'
+ANIM_COMPILE_CMD = 'convert -colorspace gray -loop 256 -delay 5 {1} -delay 300 {2} -layers optimize samples/thumbs/{0}_fr.gif'
 
-dots = []
-for dot_file in os.scandir(DOTS_PATH):
-    filename = dot_file.name
-    if not filename.endswith(".dot"):
-        continue
-    dots.append(DOTS_PATH + filename)
-dots.sort()
-
-if not os.path.exists(PNGS_PATH):
-    os.makedirs(PNGS_PATH)
-
-if not os.path.exists(THUMBS_PATH):
-    os.makedirs(THUMBS_PATH)
-
-def gen_fr(dot):
-    print(dot + " fr")
-    png = PNGS_PATH + name + "_fr.png"
-    cmd = FR_CMD.format(dot, png)
+def gen_fr(name):
+    print(name + " fr")
+    cmd = FR_CMD.format(name)
     subprocess.run([cmd], shell=True)
-    return png
+    gen_thumb(name + '_fr')
 
-def gen_kk(dot):
-    print(dot + " kk")
-    png = PNGS_PATH + name + "_kk.png"
-    cmd = KK_CMD.format(dot, png)
+def gen_kk(name):
+    print(name + " kk")
+    cmd = KK_CMD.format(name)
     subprocess.run([cmd], shell=True)
-    return png
+    gen_thumb(name + '_kk')
 
-def gen_graphviz(dot):
-    print(dot + " graphviz")
-    png = PNGS_PATH + name + "_gv.png"
-    cmd = GV_CMD.format(dot, png)
+def gen_graphviz(name):
+    print(name + " graphviz")
+    cmd = GV_CMD.format(name)
     subprocess.run([cmd], shell=True)
-    cmd = GV_RESIZE_CMD.format(png, png)
+    cmd = GV_RESIZE_CMD.format(name)
     subprocess.run([cmd], shell=True)
-    return png
+    gen_thumb(name + '_gv')
 
-def gen_fr_anim(dot):
-    print(dot + " anim")
-    frame_model = THUMBS_PATH + name + "_frame.png"
-    cmd = ANIM_CMD.format(dot, frame_model)
+def gen_thumb(name):
+    cmd = THUMB_CMD.format(name)
     subprocess.run([cmd], shell=True)
 
-    # identify frame files
-    frames = []
-    frame_regexp = re.compile(name + r"_frame_\d{3}.png")
-    for frame_file in os.scandir(THUMBS_PATH):
-        filename = frame_file.name
-        if not frame_regexp.match(filename):
+def gen_fr_anim(name):
+    print(name + " anim")
+    # generate all frames
+    cmd = ANIM_CMD.format(name)
+    subprocess.run([cmd], shell=True)
+
+    # identify frames
+    frame_names = []
+    for file in os.scandir('samples/'):
+        if not re.match(name + r'_frame_\d{3}.png', file.name):
             continue
-        frame = THUMBS_PATH + filename
-        frames.append(frame)
+        frame_name = file.name[:-len('.png')]
+        frame_names.append(frame_name)
 
     # scale down all frames to thumbnail format
-    for frame in frames:
-        cmd = THUMB_CMD.format(frame, frame)
-        subprocess.run([cmd], shell=True)
-    frames.sort()
+    for frame_name in frame_names:
+        gen_thumb(frame_name)
+        os.unlink('samples/' + frame_name + '.png')
 
-    # compile all frames in 1 gif
-    anim = THUMBS_PATH + name + "_fr.gif"
-    cmd = ANIM_COMPILE_CMD.format(' '.join(frames), frames[-1], anim)
+    # compile all frames in one gif file
+    frame_names.sort()
+    frame_thumbs = ['samples/thumbs/' + fn + '.png' for fn in frame_names]
+    cmd = ANIM_COMPILE_CMD.format(name, ' '.join(frame_thumbs), frame_thumbs[-1])
     subprocess.run([cmd], shell=True)
 
-    # delete frame images
-    for frame in frames:
-        os.unlink(frame)
+    # delete frames thumb images
+    for frame_thumb in frame_thumbs:
+        os.unlink(frame_thumb)
 
-    return anim
+def main():
+    if not os.path.exists('samples/thumbs/'):
+        os.makedirs('samples/thumbs/')
 
-def gen_thumb(png):
-    filename = os.path.basename(png)
-    thumb = THUMBS_PATH + filename
-    cmd = THUMB_CMD.format(png, thumb)
-    subprocess.run([cmd], shell=True)
-    return thumb
+    for file in os.scandir('samples/'):
+        if not file.name.endswith('.dot'):
+            continue
 
-for dot in dots:
-    name = os.path.splitext(os.path.basename(dot))[0]
+        name = file.name[:-len('.dot')]
 
-    fr_png = gen_fr(dot)
-    gen_thumb(fr_png)
+        gen_fr(name)
+        if not "large" in name:
+            gen_kk(name)
+        gen_graphviz(name)
+        gen_fr_anim(name)
 
-    if not "large" in name:
-        kk_png = gen_kk(dot)
-        gen_thumb(kk_png)
-
-    gv_png = gen_graphviz(dot)
-    gen_thumb(gv_png)
-
-    gen_fr_anim(dot)
+main()
